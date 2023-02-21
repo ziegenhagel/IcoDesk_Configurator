c942a43daf97254192b56d453659f73fe3076bcc
Dominik <dominik.ziegenhagel@gmail.com>
2023-02-21 01:54:25 +0100

    favicon and headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+    app: {
+        head: {
+            title: 'IcoDesk Configurator',
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            ],
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/ui/favicon.png'}
+            ]
+        },
+    },
 })
